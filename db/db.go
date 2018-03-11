a4b9aa06ba94084d8e88791729bcd03cf488e0c6
Daniel Ortega I <daniel@gatero.mx>
2018-03-11 11:18:18 -0600

    [db]: fix sintax od db package

--- db/db.go
+++ db/db.go
@@ -9,18 +9,20 @@ import (
 	mgo "gopkg.in/mgo.v2"
 )
 
-var MYSQL_HOST string = os.Getenv("MYSQL_CONTAINER")
-var MYSQL_DATABASE string = os.Getenv("MYSQL_DATABASE")
-var MYSQL_PORT string = os.Getenv("MYSQL_PORT")
-var MYSQL_USERNAME string = os.Getenv("MYSQL_USERNAME")
-var MYSQL_PASSWORD string = os.Getenv("MYSQL_ROOT_PASSWORD")
-var MYSQL_CONNECTION_STRING string = fmt.Sprintf(
-	"%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8",
-	MYSQL_USERNAME,
-	MYSQL_PASSWORD,
-	MYSQL_HOST,
-	MYSQL_PORT,
-	MYSQL_DATABASE,
+var (
+	MYSQL_HOST              string = os.Getenv("MYSQL_CONTAINER")
+	MYSQL_DATABASE          string = os.Getenv("MYSQL_DATABASE")
+	MYSQL_PORT              string = os.Getenv("MYSQL_PORT")
+	MYSQL_USERNAME          string = os.Getenv("MYSQL_USERNAME")
+	MYSQL_PASSWORD          string = os.Getenv("MYSQL_ROOT_PASSWORD")
+	MYSQL_CONNECTION_STRING string = fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8",
+		MYSQL_USERNAME,
+		MYSQL_PASSWORD,
+		MYSQL_HOST,
+		MYSQL_PORT,
+		MYSQL_DATABASE,
+	)
 )
 
 func OpenMySQL() (*gorm.DB, error) {
