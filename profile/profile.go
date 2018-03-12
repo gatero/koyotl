bff8d90f4677a8b0c2b902738f92c578571e31a6
Daniel Ortega I <daniel@gatero.mx>
2018-03-12 10:54:11 -0600

    [profile]: fix model configuration

--- profile/profile.go
+++ profile/profile.go
@@ -6,24 +6,46 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	mgo "gopkg.in/mgo.v2"
+	bson "gopkg.in/mgo.v2/bson"
 )
 
 type Model struct{}
 
 type Profile struct {
+	Id        bson.ObjectId `bson:"_id,omitempty"`
 	FirstName string
 	LastName  string
 	Birthday  string
-	Email     []string
 }
 
 func (m *Model) Create(c *gin.Context) {
+	mongo := db.Mongo{}
+	session, _ := mongo.Open()
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	p := session.DB("Admin").C("profiles")
+
+	profile := &Profile{
+		bson.NewObjectId(),
+		"hola mundo",
+		"hola mundo",
+		"hola mundo",
+	}
+
+	err := p.Insert(profile)
+	if err != nil {
+		panic(err)
+	}
+
 	c.String(http.StatusOK, "hello")
 }
 
 func (m *Model) Find(c *gin.Context) {
 	mongo := db.Mongo{}
 	session, _ := mongo.Open()
+	defer session.Close()
+
 	fmt.Printf("%s", session)
 	c.String(http.StatusOK, "hello")
 }
