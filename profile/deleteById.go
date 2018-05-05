8e4a589810582e7f1b2add02521f2e196049cf39
Daniel Ortega I <daniel@gatero.mx>
2018-05-05 15:29:09 -0500

    rebuild upsert method

--- profile/deleteById.go
+++ profile/deleteById.go
@@ -1,24 +1,18 @@
 package profile
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
 // RH_Delete method removes a profile instance using the uid pased through
 // URL params
-func RH_DeleteById(c *gin.Context) {
-	var update map[string]interface{}
-	update["status"] = STATUS_DELETED
+//func RH_DeleteById(c *gin.Context) {
+//var update map[string]interface{}
+//update["status"] = STATUS_DELETED
 
-	i, e := Upsert(c.Param("id"), update)
-	if e != nil {
-		c.JSON(http.StatusInternalServerError, e)
-	}
+//i, e := Upsert(c.Param("id"), update)
+//if e != nil {
+//c.JSON(http.StatusInternalServerError, e)
+//}
 
-	c.JSON(http.StatusOK, i)
-}
+//c.JSON(http.StatusOK, i)
+//}
 
 //func (rpc *RPC) FindById(ctx context.Context, p *pb.Profile) (*pb.Profile, error) {
 //return nil, grpc.Errorf(codes.Internal, "hola")
--- profile/update.go
+++ profile/update.go
@@ -1,29 +1,33 @@
 package profile
 
 import (
-	mgo "gopkg.in/mgo.v2"
+	pb "app/grpc"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	bson "gopkg.in/mgo.v2/bson"
 )
 
-func Upsert(id string, upsert interface{}) (*mgo.ChangeInfo, error) {
+func Upsert(id string, upsert *pb.Profile) error {
 	c, _ := Collection()
 
 	selector := bson.M{
 		"_id": bson.ObjectIdHex(id),
 	}
 
-	i, e := c.Upsert(selector, upsert)
+	_, e := c.Upsert(selector, &upsert)
 	if e != nil {
-		return nil, e
+		return e
 	}
-	return i, nil
+	return nil
 }
 
-//func (rpc *RPC) Upsert(ctx context.Context, p *pb.Profile) (*mgo.ChangeInfo, error) {
-//i, e := Upsert(p.Id, p)
-//if e != nil {
-//return nil, grpc.Errorf(codes.Internal, e.Error())
-//}
+func (rpc *RPC) Upsert(ctx context.Context, p *pb.Profile) (*pb.Profile, error) {
+	e := Upsert(p.Id, p)
+	if e != nil {
+		return nil, grpc.Errorf(codes.Internal, e.Error())
+	}
 
-//return i, nil
-//}
+	return p, nil
+}
