4ec423b1249e8a429e6a3301c63cd5ae4090c9c2
Daniel Ortega I <daniel@gatero.mx>
2018-05-05 20:01:36 -0500

    uncomment deletebyid method

--- profile/deleteById.go
+++ profile/deleteById.go
@@ -1,23 +1,18 @@
 package profile
 
-// RH_Delete method removes a profile instance using the uid pased through
-// URL params
-//func RH_DeleteById(c *gin.Context) {
-//var update map[string]interface{}
-//update["status"] = STATUS_DELETED
+import (
+	pb "app/grpc"
 
-//i, e := Upsert(c.Param("id"), update)
-//if e != nil {
-//c.JSON(http.StatusInternalServerError, e)
-//}
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+)
 
-//c.JSON(http.StatusOK, i)
-//}
+//RH_Delete method removes a profile instance using the uid pased through
+//URL params
+func DeleteById(id string) {
+}
 
-//func (rpc *RPC) FindById(ctx context.Context, p *pb.Profile) (*pb.Profile, error) {
-//return nil, grpc.Errorf(codes.Internal, "hola")
-//}
-
-//func (rpc *RPC) DeleteById(ctx context.Context, p *pb.Profile) (*pb.Profile, error) {
-//return nil, grpc.Errorf(codes.Internal, "hola")
-//}
+func (rpc *RPC) DeleteById(ctx context.Context, p *pb.Profile) (*pb.Profile, error) {
+	return nil, grpc.Errorf(codes.Internal, "hola")
+}
--- profile/find.go
+++ profile/find.go
@@ -2,29 +2,27 @@ package profile
 
 import (
 	pb "app/grpc"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
 // Find method get all the user instances
 // that exist into the database
-func Find(query *pb.Profile, p []*pb.Profile) error {
-	// get the collection pointer
-	c, _ := Collection()
+//func Find(query *pb.Profile, p []*pb.Profile) error {
+//// get the collection pointer
+//c, _ := Collection()
 
-	if e := c.Find(query).All(p); e != nil {
-		// if an error is ocurred then the
-		// return the corresponding error
-		return e
-	}
+//if e := c.Find(query).All(p); e != nil {
+//// if an error is ocurred then the
+//// return the corresponding error
+//return e
+//}
 
-	return nil
+//return nil
+//}
+
+func (rpc *RPC) Find(ctx context.Context, p *pb.Profile) ([]*pb.Profile, error) {
+	return []*pb.Profile{}, grpc.Errorf(codes.Internal, "hola")
 }
-
-//func (rpc *RPC) Find(ctx context.Context, p *pb.Profile) ([]*pb.Profile, error) {
-//var profiles []*pb.Profile
-
-//if e := Find(p, profiles); e != nil {
-//return nil, grpc.Errorf(codes.Internal, e.Error())
-//}
-
-//return profiles, nil
-//}
