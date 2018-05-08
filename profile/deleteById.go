82af73a6deed2ecd87d6845cbdb2a8e20a0b15ad
Daniel Ortega I <daniel@gatero.mx>
2018-05-08 14:44:42 -0500

    remove useless code

--- profile/deleteById.go
+++ profile/deleteById.go
@@ -8,11 +8,6 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
-//RH_Delete method removes a profile instance using the uid pased through
-//URL params
-func DeleteById(id string) {
-}
-
 func (rpc *RPC) DeleteById(ctx context.Context, p *pb.DeleteProfile) (*pb.Profile, error) {
 	profile := &pb.Profile{Status: STATUS_DELETED}
 	if e := Update(p.Id, profile); e != nil {
