49fda74dae01721eaf1db9f5e20e5962083785a6
Daniel Ortega I <daniel@gatero.mx>
2018-05-20 22:41:32 -0500

    remove mestli submodule

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,9 @@
-.mestli/docker/Dockerfile
+FROM golang:1.9.2
+MAINTAINER @gatero <me@daniel-ortega.mx>
+WORKDIR /go/src/app
+COPY ./entry-point.sh /usr/local/bin
+RUN go-wrapper download github.com/golang/dep/cmd/dep && \
+    go-wrapper install github.com/golang/dep/cmd/dep && \
+    go-wrapper download github.com/markbates/refresh && \
+    go-wrapper install github.com/markbates/refresh
+CMD [".gocker/docker/entry-point.sh"]
--- entry-point.sh
+++ entry-point.sh
@@ -1,1 +1,20 @@
-.mestli/docker/entry-point.sh
+#!/bin/bash
+
+LOCKFILE="./vendor/.lockfile"
+MANIFEST="Gopkg.toml"
+
+if [ ! -e Gopkg.toml ]; then
+  dep init -v
+  cp $MANIFEST $LOCKFILE > /dev/null 2>&1
+fi
+
+if [ -d ./vendor ]; then
+  if ! diff $MANIFEST $LOCKFILE > /dev/null 2>&1; then
+    dep ensure
+    cp $MANIFEST $LOCKFILE > /dev/null 2>&1
+  fi
+fi
+
+printf "%b" "${PURPLE_BOLD}ENTRY POINT RUNING${NO_COLOR}"
+
+refresh run
